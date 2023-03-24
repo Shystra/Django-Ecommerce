df6b1a8bab9bdf21c564ab0900c460cdb4a61d9c
Shystra <jlsp.2019@hotmail.com>
2023-03-24 11:29:40 -0300

    Puxando caminho de imagem Produto

--- loja/settings.py
+++ loja/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'produto.apps.ProdutoConfig',
 
     #TODO REMOVER DEBUG TOOLBAR
     'debug_toolbar',
--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+import os
+from django.conf import settings
+from PIL import Image
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField (max_length = 255)
+
+    descricao_cruta = models.TextField (max_length = 255)
+    descricao_longa = models.TextField ()
+    imagem = models.ImageField(
+        upload_to = 'produto_imagens/%Y/%m/', blank = True, null = True
+    )
+    slug = models.SlugField (unique = True)
+    preco_marketing = models.FloatField (default = 0)
+    preco_marketing_promocional = models.FloatField (default = 0)
+
+    #TODO Para criar o botao de variação segue modelo abaixo
+    tipo = models.CharField ( 
+        default = 'V',
+        max_length = 1, 
+        choices = (
+        
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        ) 
+    )
+
+    @staticmethod
+    def resize_image (img, new_width = 800):
+        img_full_path = os.path.join (settings.MEDIA_ROOT, img.name)
+        print(img_full_path)
+
+    #TODO SALVA IMAGEM E ENVIA
+    def save (self, *args, **kwargs):
+        super().save (*args, **kwargs)
+
+    #TODO SE A IMAGEM FOR ENVIADA ELE REDIMENCIONA 
+        max_image_size = 800
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
+
+    #TODO Retorna o nome do produto criado
+    def __str__ (self):
+        return self.nome
+    
