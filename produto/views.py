d9f059b7609ee008a79b18fc985bcb76d4d706e6
Shystra <jlsp.2019@hotmail.com>
2023-03-25 12:43:10 -0300

    Ajustando views e urls dos apps

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+class ListaProdutos (View):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+
+
+
+
+
+
+
+
+class DetalheProduto (View):
+    def get (self, *args, **kwargs):
+        return HttpResponse ('Detalhe Produto')
+
+class AdicionarAoCarrinho (View):
+    def get (self, *args, **kwargs):
+        return HttpResponse ('Adicionar carrinho')
+
+class RemoverDoCarrinho (View):
+    def get (self, *args, **kwargs):
+        return HttpResponse ('Remover Carrinho')
+
+
+class Carrinho (View):
+    def get (self, *args, **kwargs):
+        return HttpResponse ('Carrinho')
+
+
+class Finalizar (View):
+    def get (self, *args, **kwargs):
+        return HttpResponse ('Finalizar')
+
