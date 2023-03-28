e9c1a68854bbd0417c99d812658b903100ba9973
Shystra <jlsp.2019@hotmail.com>
2023-03-28 09:41:11 -0300

    Modificando o Produto "ADICIONAR AO CARRINHO"

--- produto/views.py
+++ produto/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views import View
 from django.views.generic.detail import DetailView
 from django.http import HttpResponse
+from django.contrib import messages
 from . import models
 
 class ListaProdutos (ListView):
@@ -27,30 +28,58 @@ class DetalheProduto (DetailView):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class AdicionarAoCarrinho (View):
-    def get (self, *args, **kwargs):
-        return HttpResponse ('Adicionar carrinho')
+    def get (self, *args, **kwargs):        
+        http_refere = self.request.META.get ('HTTP_REFERER', reverse ('produto:lista') )
+        variacao_id = self.request.GET.get ('vid')
+
+        if not variacao_id:
+            messages.error (
+                self.request, 
+                'Produto não existe'
+            )
+            return redirect (http_refere)
+        
+
+        variacao = get_object_or_404 (models.Variacao, id = variacao_id)
+
+
+        if not self.request.session.get ('carrinho'):
+            self.request.session ['carrinho'] = {}
+            self.request.session.save ()
+
+        carrinho = self.request.session ['carrinho']
+
+        if variacao_id in carrinho:
+            #TODO: Variação existe no carrinho
+            pass
+
+        else:
+            #TODO: Variação nao existe no carrinho
+            pass
+
+
+
+        return HttpResponse (f'{variacao.produto} {variacao.nome}')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class RemoverDoCarrinho (View):
     def get (self, *args, **kwargs):
