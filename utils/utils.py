8e556665b6c0912cac0e89574baea608fda2602a
Shystra <jlsp.2019@hotmail.com>
2023-03-29 20:55:18 -0300

    Ajustando total do carrinho

--- utils/utils.py
+++ utils/utils.py
@@ -5,3 +5,16 @@ def formata_preco (val):
 #TODO Soma a quantidade para cada item no carrinho.
 def cart_total_qtd (carrinho):
     return sum([item ['quantidade'] for item in carrinho.values ()])
+
+
+def cart_totals (carrinho):
+    return sum (
+        [
+            #TODO Obtenha o preco_quantitativo_promocional se o preco_quantitativo_promocional estiver preenchido
+            #TODO Ao contrario disso obtenha o preco_quantitativo para cada item no carrinho
+            item.get ('preco_quantitativo_promocional')
+            if item.get ('preco_quantitativo_promocional')
+            else item.get ('preco_quantitativo')
+            for item in carrinho.values ()
+        ]
+    )
